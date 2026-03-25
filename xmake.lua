7dfd1665779c0b1624f387932145f4c6a9484e43
AlvinQID <github@alvinq.my.id>
2026-03-25 11:25:01 +0700

    Refactor xmake.lua for improved file structure

--- xmake.lua
+++ xmake.lua
@@ -6,20 +6,17 @@ set_languages("cxx23")
 add_rules("mode.release")
 
 add_repositories(
-    "xmake-repo https://github.com/xmake-io/xmake-repo.git",
-    "liteldev-repo https://github.com/LiteLDev/xmake-repo.git"
+    "xmake-repo https://github.com/xmake-io/xmake-repo.git"
 )
 
-add_requires("nlohmann_json v3.11.3")
 
 target("BetterTooltips")
     set_kind("shared")
-    add_packages("nlohmann_json")
     add_linkdirs("niseAPI/libs/arm64-v8a")
     add_linkdirs("miniAPI/libs/arm64-v8a")
     add_links("nise", "log", "miniAPI", "GlossHook")
 
-    add_files("src/*.cpp", "niseAPI/deps/gamepwnage/src/*.c", "niseAPI/deps/gamepwnage/src/**/*.c")
+    add_files("src/*.cpp", "src/**/*.cpp", "niseAPI/deps/gamepwnage/src/*.c", "niseAPI/deps/gamepwnage/src/**/*.c")
 
     add_includedirs("include","miniAPI/include", "niseAPI/include", "niseAPI/deps/gamepwnage/includes", {public = true})
 
